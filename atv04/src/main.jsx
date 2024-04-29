46023ecb433785cf5943b5dd073bdb7b6db875b3
NewZayn <97660788+NewZayn@users.noreply.github.com>
2024-04-29 23:26:58 +0000

    fianl commit

--- atv04/src/main.jsx
+++ atv04/src/main.jsx
@@ -1,11 +1,35 @@
-/* existing imports */
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
 import Root from "./routes/root";
+
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ]
   },
+
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
+  },
+  
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
